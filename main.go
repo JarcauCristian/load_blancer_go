2f1793209e43c4ed30cdb50494d0943806b18a0e
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2024-03-03 15:39:45 +0000

    Changed Some things

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ func main() {
 	}
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.MaxMultipartMemory = 100 << 20
 
 	r.Use(CORSMiddleware())
 
@@ -622,7 +621,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
